46af287c69a55afe9208c22c474ff13c4c3cacc8
Patrick Gross <142960751+BearsLair@users.noreply.github.com>
2025-07-10 01:05:33 -0700

    created basic product list for testing

---
+++ ShoppingSite/src/components/ProductsList.jsx
@@ -0,0 +1,13 @@
+import ProductCard from "./ProductCard.jsx";
+
+function ProductList() {
+  return (
+    <div className="product-grid">
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+    </div>
+  );
+}
+
+export default ProductList;
